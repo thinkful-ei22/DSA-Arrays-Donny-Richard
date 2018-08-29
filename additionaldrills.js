838ec254a32d0cc76e40617d33078ac22c8d992d
Richard Tom <tomrichard14@gmail.com>
2018-08-29 13:57:04 -0700

    drills finished

--- additionaldrills.js
+++ additionaldrills.js
@@ -129,8 +129,94 @@ console.log(removeCharacters('Battle of the Vowels: Hawaii vs. Grozny', 'aeiou')
 
 //linear time complexity O(n)
 
-
-
 //Products
+// Input:[1, 3, 9, 4]
+// Output:[108, 36, 12, 27]
+const inputArray = [1, 3, 9, 4];
 
+const products = array => {
+  let returnArray = [];
+  for(let i=0; i < array.length; i++) {
+    let returnProduct = 1;
+    for(let j=0; j < i; j++){
+      returnProduct *= array[j];
+    }
+    for(let k=i+1; k < array.length; k++) {
+      returnProduct *= array[k];
+    }
+    returnArray.push(returnProduct);
+  }
+  return returnArray;
+};
 
+console.log(products(inputArray));
+
+// polynomial complexity O(n^2)
+
+const input2DArray = 
+  [[1,0,1,1,0],
+    [0,1,1,1,0],
+    [1,1,1,1,1],
+    [1,0,1,1,1],
+    [1,1,1,1,1]];
+
+// [[0,0,0,0,0],
+// [0,0,0,0,0],
+// [0,0,1,1,0],
+// [0,0,0,0,0],
+// [0,0,1,1,0]];
+
+function arrayClone( arr ) {
+
+  var i, copy;
+
+  if( Array.isArray( arr ) ) {
+    copy = arr.slice( 0 );
+    for( i = 0; i < copy.length; i++ ) {
+      copy[ i ] = arrayClone( copy[ i ] );
+    }
+    return copy;
+  } else if( typeof arr === 'object' ) {
+    throw 'Cannot clone array containing an object!';
+  } else {
+    return arr;
+  }
+
+}
+
+const array2d = array => {
+  
+  const newArray = arrayClone(array);
+  for(let i=0; i < array.length; i++) {
+    for(let j=0; j < array[i].length; j++) {
+      if(array[i][j] === 0) {
+        for(let x=0; x<array[i].length; x++) {
+          newArray[i][x] = 0;
+        }
+        for(let y=0; y<array.length; y++) {
+          newArray[y][j] = 0;
+        }
+      }
+    }
+  }
+  return newArray;
+};
+
+// console.log(array2d(input2DArray));
+
+//polynomial O(n^3)
+
+const stringRotate = (str1, str2) => {
+  // return (str2 + str2).includes(str1);
+  const cirlceString = str2+str2;
+  for(let i=0; i < cirlceString.length - str1.length; i++) {
+    if (cirlceString.slice(i,i+str1.length) === str1) {
+      return true;
+    }
+  }
+  return false;
+};
+
+//complexity O(1)
+console.log(stringRotate('amazon', 'azonam'));
+console.log(stringRotate('amazon', 'azonma'));
